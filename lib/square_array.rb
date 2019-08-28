bb364a78ff51972f3e88c6418c0f1b1cfecee134
Huy Pham <benjaminhuypham@gmail.com>
2019-08-28 05:46:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,6 @@
-def square_array(array)
-    # your code here
-    puts array.map{|n| n*n}
-end
-
-square_array([1,2,3,4])
+    new_array = []
+    i = 0 
+    while i < array.length do 
+        new_array.push(array[i]*array[i])
+        i+=1
+    end
